a1c939fc2366bee09d9210e605e1c9fe34696d0e
Marc-Olivier <marc.olivier.p.s@gmail.com>
2020-05-07 17:13:02 -0400

    Updates

---
+++ precache-manifest.26cfad1c8cba91c18e240c2f5c3d33f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce985a6931df5d291ca8220321f294c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "89a9a6d2a9eb2bfda2c0",
+    "url": "/monsters-rolodex/static/css/main.4f6f277e.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a9a6d2a9eb2bfda2c0",
+    "url": "/monsters-rolodex/static/js/main.32916787.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
